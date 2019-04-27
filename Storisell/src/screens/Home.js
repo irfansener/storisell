ec11053d7b84da0034b3dff3a9f49acece01d31a
Irfan Sener <irfansener00@gmail.com>
2019-04-27 12:12:41 +0300

    Added native-base

---
+++ Storisell/src/screens/Home.js
@@ -0,0 +1,29 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+class Home extends Component {
+    static navigationOptions = {
+        title: 'Home'
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Home</Text>
+            </View>
+        );
+    }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+//make this component available to the app
+export default Home;
