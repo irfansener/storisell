1cb1b5e5ba395d1f0a57dcfd1e11c40d41231412
Irfan Sener <irfansener00@gmail.com>
2019-04-27 16:13:04 +0300

    Added theme four

--- Storisell/src/components/ThemeFour.js
+++ Storisell/src/components/ThemeFour.js
@@ -1,13 +1,32 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import GlobalStore from '../stores/GlobalStore';
+import { observer } from 'mobx-react/native';
 
-// create a component
+
 class ThemeFour extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>ThemeFour</Text>
+                <View style={styles.leftSide}>
+                </View>
+                <View style={styles.rightSide}>
+                </View>
+                <Text style={styles.price}>
+                    {GlobalStore.linkData.price}
+                </Text>
+                <Image
+                    style={styles.imageOne}
+                    source={{ uri: GlobalStore.linkData.pictures[0] }}
+                />
+                <Image
+                    style={styles.imageTwo}
+                    source={{ uri: GlobalStore.linkData.pictures[1] }}
+                />
+                <Text style={styles.title}>
+                    {GlobalStore.linkData.title}
+                </Text>
             </View>
         );
     }
@@ -22,7 +41,51 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         width: '48%',
         height: '48%',
-        margin: '0.5%'
+        margin: '0.5%',
+        flexDirection: 'row',
+    },
+    leftSide: {
+        backgroundColor: '#DFA3A5',
+        width: '70%',
+        height: '100%',
+    },
+    rightSide: {
+        backgroundColor: '#FFFFFF',
+        width: '30%',
+        height: '100%',
+    },
+    price: {
+        position: 'absolute',
+        top: '5%',
+        left: '5%',
+        color: '#fff',
+        fontWeight: '800',
+    },
+    imageOne: {
+        width: '80%',
+        height: '30%',
+        position: 'absolute',
+        top: '15%',
+        left: '5%',
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+    imageTwo: {
+        width: '80%',
+        height: '30%',
+        position: 'absolute',
+        top: '48%',
+        right: 0,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+    title: {
+        position: 'absolute',
+        bottom: '10%',
+        left: '20%',
+        color: '#fff',
+        fontWeight: '600',
+        fontSize: 20,
     },
 });
 
--- Storisell/src/helper/Api.js
+++ Storisell/src/helper/Api.js
@@ -2,8 +2,8 @@ class Api {
     getUrlValues(url) {
         return {
             pictures: [
-                'https://s3.eu-central-1.amazonaws.com/shopier1/pictures_large/cocoonsoul6106_D5383F5A-50BB-493A-A014-CBD8CCBCB628.jpeg',
-                'https://s3.eu-central-1.amazonaws.com/shopier1/pictures_large/cocoonsoul6106_D5383F5A-50BB-493A-A014-CBD8CCBCB628.jpeg'
+                'https://s3.eu-central-1.amazonaws.com/shopier1/pictures_large/Camiseta1589_t-shirts1.jpg',
+                'https://s3.eu-central-1.amazonaws.com/shopier1/pictures_large/Camiseta1356_purple-t-shirt.jpg'
             ],
             title: 'T-shirt',
             price: '40,00 TL'
